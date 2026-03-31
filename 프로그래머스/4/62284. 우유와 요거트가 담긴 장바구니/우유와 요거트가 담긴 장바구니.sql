962fcea88479d6371ede94f496cdf29941837e37
Gyubuk <g1370200@gmail.com>
2026-04-01 01:08:18 +0900

    [level 4] Title: 우유와 요거트가 담긴 장바구니, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
+++ 프로그래머스/4/62284. 우유와 요거트가 담긴 장바구니/우유와 요거트가 담긴 장바구니.sql
@@ -2,11 +2,6 @@
 --  우유(Milk)와 요거트(Yogurt)를 동시에 구입한 장바구니가 있는지 알아보려 합니다
 -- 우유와 요거트를 동시에 구입한 장바구니의 아이디를 조회하는 SQL 문
 -- 이때 결과는 장바구니의 아이디 순
--- SELECT CART_ID
--- FROM CART_PRODUCTS
--- GROUP BY CART_ID
-
-
 -- CASE WHEN으로 있으면 뭐 없으면 뭐로 나눌 수 있는데 SELECT가 되니까
 -- 내 생각엔 서브 쿼리로 만드는 방향이 맞을거 같음.
 
