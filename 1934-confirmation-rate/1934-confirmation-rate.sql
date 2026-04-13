a893ad7ff568a53fc3f56c88bb9ff762c45cefe9
Gyubuk <g1370200@gmail.com>
2026-04-13 22:38:58 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,8 +1,5 @@
 -- Write your PostgreSQL query statement below
 
-
-
-
 SELECT S.user_id, ROUND(1.0*SUM(CASE WHEN ACTION = 'confirmed' THEN 1 ELSE 0 END) / COUNT(*), 2) AS confirmation_rate
 FROM Signups S
 LEFT JOIN Confirmations C ON S.USER_ID = C.USER_ID
